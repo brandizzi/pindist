9cc47188c58f143fae7cad4e4e1e359745f004b5
Adam Victor Nazareth Brandizzi <brandizzi@gmail.com>
2012-08-01 01:54:27 -0300

    Better interface

--- page.pindist.php
+++ page.pindist.php
@@ -22,24 +22,28 @@
  */
 $action = @$_REQUEST['action'];
 $pinset_id = @$_REQUEST['pinset']; 
-if (!$action && !$pinset_id) { ?>
+?>
+<div class="rnav">
+<ul>
+<?php foreach (pindist_get_pin_sets() as $pinset) { ?>
+    <li>
+        <a href="?display=pindist&pinset=<?php echo $pinset['id']; ?>">
+            <?php echo $pinset['description']; ?>
+        </a>
+    </li> 
+<?php } ?>
+</ul>
+</div>
+<h2>PIN Distribution</h2>
+<?php if (!$action && !$pinset_id) { ?>
 <div class="pindist pinsets">
-    <h2>PIN sets</h2>
-    <ul>
-    <?php foreach (pindist_get_pin_sets() as $pinset) { ?>
-        <li>
-            <a href="?display=pindist&pinset=<?php echo $pinset['id']; ?>">
-                <?php echo $pinset['description']; ?>
-            </a>
-        </li> 
-    <?php } ?>
-    </ul>
+    <p>Those are the available PIN sets. Choose one and manage its PINs</p>
 </div>
 <?php } else if (!$action && $pinset_id) { ?>
 <div class="pindist pins">
-    <h2>PINs</h2>
+    <h3>PINs</h3>
     <div class="available" style="float:left; width: 25%;">
-    <h3 style="margin-top: 0em;">Available</h3>
+    <h4 style="margin-top: 0em;">Available</h4>
     <table sytle="border: 1px solid #999; background-color: #DDD; text-align: center;">
     <tr>
         <th style="background-color: #AAA; margin: 1em;">PIN</th><th style="background-color: #AAA; margin: 1em;">Associate</th>
@@ -59,23 +63,32 @@ if (!$action && !$pinset_id) { ?>
     </table>
     </div>
     <div class="attributed" style="margin-left: 30%; width: 60%;"/>
-    <h3>Attributed</h3>
+    <h4>Attributed</h4>
     <table>
     <tr>
         <th style="background-color: #AAA; margin: 1em;">PIN</th>
         <th style="background-color: #AAA; margin: 1em;">Person</th>
-        <th style="background-color: #AAA; margin: 1em;">Disassociate</th>
+        <th style="background-color: #AAA; margin: 1em;">Free</th>
+        <th style="background-color: #AAA; margin: 1em;">Revoke</th>
     </tr>
     <?php foreach (pindist_get_pin_associations($pinset_id) as $association) { ?> 
     <tr>
         <td style="border: 1px solid #999; background-color: #DDD; margin: 1em; padding: 0 1em 0 1em;"><?php echo $association['pin']; ?> </td>
         <td style="border: 1px solid #999; background-color: #DDD; margin: 1em; padding: 0 1em 0 1em;"><?php echo $association['name']; ?></td>
         <td style="border: 1px solid #999; background-color: #DDD; margin: 1em; padding: 0 1em 0 1em;">
-        <a href="?display=pindist&action=revoke&pin=<?php echo $association['pin']; ?>&pinset=<?php echo $pinset_id; ?>">
+        <a href="?display=pindist&action=disassociate&pin=<?php echo $association['pin']; ?>&pinset=<?php echo $pinset_id; ?>">
         <?php $pin = $association['pin']; $name = $association['name']; ?>
             <img src="/admin/images/user_delete.png" 
-                    alt="Disassociate PIN <?php echo $pin; ?> from <?php echo $pin; ?>"
-                    title="Disassociate PIN <?php echo $pin; ?> from <?php echo $pin; ?>">
+                    alt="Disassociate PIN <?php echo $pin; ?> from <?php echo $name; ?>"
+                    title="Disassociate PIN <?php echo $pin; ?> from <?php echo $name; ?>">
+        </a>
+        </td>
+        <td style="border: 1px solid #999; background-color: #DDD; margin: 1em; padding: 0 1em 0 1em;">
+        <a href="?display=pindist&action=revoke&pin=<?php echo $association['pin']; ?>&pinset=<?php echo $pinset_id; ?>">
+        <?php $pin = $association['pin']; $name = $association['name']; ?>
+            <img src="/admin/images/cancel.png" 
+                    alt="Revoke PIN <?php echo $pin; ?> from <?php echo $name; ?>. The PIN will be rendered invalid after that."
+                    title="Revoke PIN <?php echo $pin; ?> from <?php echo $name; ?>. The PIN will be rendered invalid after that.">
         </a>
         </td>
     </tr> 
@@ -103,8 +116,13 @@ if (!$action && !$pinset_id) { ?>
     $name = $_REQUEST['name'];
     pindist_save_association($pin, $pinset_id, $name);
     redirect_standard('pinset');
+} else if ($action == 'disassociate' && $pinset_id) {  
+    $pin = $_REQUEST['pin'];
+    pindist_disassociate_association($pin, $pinset_id);
+    redirect_standard('pinset');
 } else if ($action == 'revoke' && $pinset_id) {  
     $pin = $_REQUEST['pin'];
     pindist_revoke_association($pin, $pinset_id);
+    needreload();
     redirect_standard('pinset');
 } ?>
